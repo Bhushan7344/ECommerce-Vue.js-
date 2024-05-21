5b59c169233c3685890668c681cd6104e462ee0d
D Bhushan <gbhushand@gmail.com>
2024-05-21 17:05:28 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex p-8 justify-center">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for a Meal"
+    />
+
+    <div class="flex justify-center mt-2 gap-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+</script>
